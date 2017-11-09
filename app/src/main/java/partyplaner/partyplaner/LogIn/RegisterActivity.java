c7347773a3cbfa08da553dd7366d7787df8c2670
Malte Riechmann <malte.riechmann@fh-bielefeld.de>
2017-11-09 16:18:52 +0100

    Toast fuer erfolgreiche Registration eingefuegt

--- app/src/main/java/partyplaner/partyplaner/LogIn/RegisterActivity.java
+++ app/src/main/java/partyplaner/partyplaner/LogIn/RegisterActivity.java
@@ -1,9 +1,11 @@
 package partyplaner.partyplaner.LogIn;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import partyplaner.partyplaner.MainActivity;
 import partyplaner.partyplaner.R;
@@ -17,6 +19,14 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     public void saveRegister(View view) {
+
+        Context context = getApplicationContext();
+        CharSequence text = "Registrieren erfolgreich";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
