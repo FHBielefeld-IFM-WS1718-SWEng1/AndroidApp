b136e2c53d2a8d115172396827f2dd7460506b3a
Malte Riechmann <malte.riechmann@fh-bielefeld.de>
2017-11-11 10:00:52 +0100

    setup des Veranstaltungsfragment

--- app/src/main/java/partyplaner/partyplaner/Veranstaltung/VeranstaltungMainFragment.java
+++ app/src/main/java/partyplaner/partyplaner/Veranstaltung/VeranstaltungMainFragment.java
@@ -1,10 +1,22 @@
 package partyplaner.partyplaner.Veranstaltung;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import partyplaner.partyplaner.R;
 
 /**
  * Created by malte on 10.11.2017.
  */
 
 public class VeranstaltungMainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_contact_form, container, false);
+    }
 }
