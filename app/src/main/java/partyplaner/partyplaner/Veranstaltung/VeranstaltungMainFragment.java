979aabfeb22322e8702559a69d33b7bf48c370f1
Malte Riechmann <malte.riechmann@fh-bielefeld.de>
2017-11-11 10:00:35 +0100

    setup des Veranstaltungsfragment

---
+++ app/src/main/java/partyplaner/partyplaner/Veranstaltung/VeranstaltungMainFragment.java
@@ -0,0 +1,10 @@
+package partyplaner.partyplaner.Veranstaltung;
+
+import android.app.Fragment;
+
+/**
+ * Created by malte on 10.11.2017.
+ */
+
+public class VeranstaltungMainFragment extends Fragment {
+}
