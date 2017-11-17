7d33e32dfdc0b926e1c450d02bf28ff0cf86d249
Unknown <malte.riechmann@fh-bielefeld.de>
2017-11-17 10:17:01 +0100

    Darstellungs Fix

--- app/src/main/java/partyplaner/partyplaner/home/HomeFragment.java
+++ app/src/main/java/partyplaner/partyplaner/home/HomeFragment.java
@@ -20,7 +20,7 @@ public class HomeFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home_party, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
         LinearLayout layout = view.findViewById(R.id.party_list);
         //layout.removeAllViews();
         return view;
