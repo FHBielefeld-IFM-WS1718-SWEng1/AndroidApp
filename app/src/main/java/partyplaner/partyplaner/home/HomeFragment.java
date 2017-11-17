65c3c36966fd17aaa41eb6ad55533176690613aa
André <akirsch1@fh-bielefeld.de>
2017-11-17 10:41:06 +0100

    Fertigstellen des Home Mockups

--- app/src/main/java/partyplaner/partyplaner/home/HomeFragment.java
+++ app/src/main/java/partyplaner/partyplaner/home/HomeFragment.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
+import java.util.Random;
+
 import partyplaner.partyplaner.R;
 
 /**
@@ -17,12 +19,38 @@ import partyplaner.partyplaner.R;
 
 public class HomeFragment extends Fragment{
 
+    private LinearLayout partyHolder;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        LinearLayout layout = view.findViewById(R.id.party_list);
-        //layout.removeAllViews();
+        partyHolder = view.findViewById(R.id.party_list);
+
+        updateParties();
+
         return view;
+    }
+
+    private void updateParties() {
+        if(partyHolder != null)
+            partyHolder.removeAllViews();
+
+        int partyCount = getPartyCount();
+
+        for (int i = 0; i <= partyCount; i++) {
+            addParty();
+        }
+    }
+
+    private void addParty() {
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.party_list, new PartyHomeFragment());
+        fragmentTransaction.commit();
+    }
+
+    private int getPartyCount() {
+        return new Random().nextInt(6) + 2;
     }
 }
