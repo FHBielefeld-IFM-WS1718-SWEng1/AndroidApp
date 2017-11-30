883027cab78cdb7c1b4767fd324dbd4add6c12bb
Jan Augstein <jaugstein@fh-bielefeld.de>
2017-11-30 21:08:49 +0100

    Einfügen einzelner Gäste in die Liste

--- app/src/main/java/partyplaner/partyplaner/Veranstaltung/Fragmente/GuestList.java
+++ app/src/main/java/partyplaner/partyplaner/Veranstaltung/Fragmente/GuestList.java
@@ -1,10 +1,14 @@
 package partyplaner.partyplaner.Veranstaltung.Fragmente;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import java.util.Random;
 
 import partyplaner.partyplaner.R;
 
@@ -13,9 +17,38 @@ import partyplaner.partyplaner.R;
  */
 
 public class GuestList extends Fragment {
+    private LinearLayout guestHolder;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.event_fragment_guestlist, container, false);
+        View view = inflater.inflate(R.layout.event_fragment_guestlist, container, false);
+        guestHolder = view.findViewById(R.id.guest_list);
+
+        updateGuests();
+
+        return view;
+    }
+
+    private void updateGuests(){
+        if(guestHolder != null){
+            guestHolder.removeAllViews();
+        }
+        int guestCount = getGuestCount();
+
+        for(int i = 0; i<=guestCount; i++){
+            addGuest();
+        }
+
+    }
+
+    private void addGuest(){
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.guest_list, new SingleGuest());
+        fragmentTransaction.commit();
+    }
+
+    private int getGuestCount(){
+        return new Random().nextInt(6) + 2;
     }
 }
