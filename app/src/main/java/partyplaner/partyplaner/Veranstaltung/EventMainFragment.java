1c96b9390161ed2152abb3b632f534fe074b24b1
Malte <malte.riechmann@fh-bielefeld.de>
2017-12-01 13:51:41 +0100

    Beschreibung der Veranstaltung hinzufügen

--- app/src/main/java/partyplaner/partyplaner/Veranstaltung/EventMainFragment.java
+++ app/src/main/java/partyplaner/partyplaner/Veranstaltung/EventMainFragment.java
@@ -7,7 +7,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,13 +28,72 @@ import partyplaner.partyplaner.Veranstaltung.Fragmente.TaskList;
  */
 public class EventMainFragment extends Fragment {
 
+    private String what = "Feier";
+    private String who = "Tim";
+    private String where = "Hier";
+    private String when = "Jetzt";
+    private String description = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+            "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+            "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    private boolean shortText = true;
+
     List<ExpandableFragment> headers = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_event_main, container, false);
+        final View view = inflater.inflate(R.layout.fragment_event_main, container, false);
+        setUpDescription(view);
         setUpExpandableView(view);
+
+        final Button more = view.findViewById(R.id.button_more);
+        more.setVisibility(View.INVISIBLE);
+
+        if (description.length() > 80) {
+            more.setVisibility(View.VISIBLE);
+            more.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    TextView description = view.findViewById(R.id.event_description);
+                    if (shortText) {
+                        description.setText(getDescription());
+                        more.setText("weniger...");
+                        shortText = false;
+                    } else {
+                        description.setText((getDescription().substring(0, 80) + "..."));
+                        more.setText("mehr...");
+                        shortText = true;
+                    }
+
+                }
+            });
+        }
         return view;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(boolean bool) {
+        shortText = bool;
+    }
+
+    private void setUpDescription(View view) {
+        TextView eventWhat = view.findViewById(R.id.event_what);
+        TextView eventWho = view.findViewById(R.id.event_who);
+        TextView eventWhere = view.findViewById(R.id.event_where);
+        TextView eventWhen = view.findViewById(R.id.event_when);
+        TextView eventDescription = view.findViewById(R.id.event_description);
+
+        eventWhat.setText(what);
+        eventWho.setText(who);
+        eventWhere.setText(where);
+        eventWhen.setText(when);
+        if(description.length() <= 80) {
+            eventDescription.setText(description);
+        } else {
+            eventDescription.setText(description.substring(0, 80) + "...");
+        }
     }
 
     private void addFragment(String name, int id) {
@@ -51,8 +112,6 @@ public class EventMainFragment extends Fragment {
     }
 
     private void setUpExpandableView(View view) {
-        LinearLayout layout = view.findViewById(R.id.eventBody);
-        layout.removeAllViews();
 
         addFragment("Gallerie", 0);
         addFragment("Aufgaben", 1);
