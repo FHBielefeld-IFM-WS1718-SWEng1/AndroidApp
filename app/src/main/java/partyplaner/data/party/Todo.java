2a885959ddcf3b021f8cc678124381d0e9788f0a
Malte <malte.riechmann@fh-bielefeld.de>
2018-01-09 22:06:26 +0100

    Kleine Layout Anpassungen

---
+++ app/src/main/java/partyplaner/data/party/Todo.java
@@ -0,0 +1,8 @@
+package partyplaner.data.party;
+
+/**
+ * Created by malte on 09.01.2018.
+ */
+
+public class Todo {
+}
---
+++ app/src/main/java/partyplaner/partyplaner/Veranstaltung/IEventDataManager.java
@@ -0,0 +1,8 @@
+package partyplaner.partyplaner.Veranstaltung;
+
+/**
+ * Created by malte on 09.01.2018.
+ */
+
+public interface IEventDataManager {
+}
