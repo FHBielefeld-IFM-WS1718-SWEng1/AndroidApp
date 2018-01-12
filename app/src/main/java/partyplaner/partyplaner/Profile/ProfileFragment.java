97fee9a416e039b0d6fc0067d1da24daec5e054a
André <akirsch1@fh-bielefeld.de>
2018-01-12 13:30:12 +0100

    Profilbild laden vom Server, ungetestet

--- app/src/main/java/partyplaner/partyplaner/Profile/ProfileFragment.java
+++ app/src/main/java/partyplaner/partyplaner/Profile/ProfileFragment.java
@@ -16,11 +16,18 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+
 import java.io.BufferedInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
+import okhttp3.Route;
+import partyplaner.api.GeneralAPIRequestHandler;
+import partyplaner.api.RouteType;
+import partyplaner.data.Base64Image;
+import partyplaner.data.PaPlaImage;
 import partyplaner.data.user.Gender;
 import partyplaner.data.user.I;
 import partyplaner.partyplaner.R;
@@ -66,13 +73,16 @@ public class ProfileFragment extends Fragment {
             }
         });
 
-        Bitmap bitmap = null;
+        Base64Image b64img = new Gson().fromJson(GeneralAPIRequestHandler.request("/images/" + profil.getProfilePicture() + "?api=" + profil.getApiKey(), RouteType.GET, null), Base64Image.class);
+        if (b64img != null && b64img.getData() != null) {
+            PaPlaImage img = new PaPlaImage(b64img.getData());
+            Bitmap bitmap = img.convertToBitmap();
 
-        profile.setImageBitmap(Bitmap.createScaledBitmap(bitmap,
-                (int)((double)bitmap.getWidth() / ((double)bitmap.getHeight() / 1024.0)),
-                1024,
-                false));
-
+            profile.setImageBitmap(Bitmap.createScaledBitmap(bitmap,
+                    (int) ((double) bitmap.getWidth() / ((double) bitmap.getHeight() / 1024.0)),
+                    1024,
+                    false));
+        }
         return view;
     }
 
